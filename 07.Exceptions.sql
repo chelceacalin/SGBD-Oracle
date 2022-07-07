e0f89cb610d25c37df5980b9cc710a4cebedcbb9
Chelcea Calin Marius <76866499+chelceacalin@users.noreply.github.com>
2022-07-07 16:07:19 +0300

    Update 07.Exceptions.sql

--- 07.Exceptions.sql
+++ 07.Exceptions.sql
@@ -1,207 +1,156 @@
---1 .Procedura modifica_salariul prime?te doi parametrii:
---p_id_instructor ?i procent ?i majoreaz? cu procentul specificat
---salariul instructorului cu acel id
+
+--1. Să se afişeze instructorul cu  id 15 Să se trateze eroarea apărută în cazul în c
+--are nu există nici un instructor cu acest id
 set serveroutput on;
-create or replace procedure ModificaSalariul(p_id_instructor instructori.id_instructor%type, procent number)
-is
-vSal instructori.sal%type;
-BEGIN
-select sal into vSal 
-from instructori
-where id_instructor=p_id_instructor;
-dbms_output.put_line('Salariul inainte de modificare: '||vSal);
-update instructori
-set sal=sal*(procent/100+1)
-where id_instructor=p_id_instructor;
-select sal into vSal from instructori where id_instructor=p_id_instructor;
-dbms_output.put_line('Salariul DUPA de modificare: '||vSal);
-END;
-/
-call ModificaSalariul(1,15);
-EXECUTE ModificaSalariul(1, 15);
-begin
- ModificaSalariul(1,15);
-end;
-/
- 
-
-
--- 2. Procedura calculeaz? salariul mediu ?i �l returneaz? printr-un parametru de tip OUT
-create or replace procedure CalcSalMediu(Psal out number)
-is
-BEGIN
-select avg(sal) into psal from instructori;
-dbms_output.put_line(Psal);
-END;
-/
-variable Vmediu number;
-call CalcSalMediu(:Vmediu);
-print Vmediu;
- 
-
---3. Procedura prime?te ca parametru de tip IN id_ul unui instructor 
---?i returneaz? prin parametrii de tip OUT numele ?i salariul acestuia:
-
-create or replace procedure Afiseaza(pID in instructori.id_instructor%type,NumeInstructor out varchar,vsal out number)
-is
-begin
-select nume,sal into NumeInstructor,vsal 
-from instructori 
-where id_instructor=pID;
-dbms_output.put_line(NumeInstructor|| ' Salariul : '||vsal);
-END;
-/
 declare
-vNume instructori.nume%type;
-vSal instructori.sal%type;
+varNume instructori.nume%type;
 begin
-Afiseaza(1,vNume,vSal);
-END;
-/
- 
---4. Modifica salariul instructorului  care are salariul mai mic decat sal mediu
-create or replace procedure Modif(Pid_Instructor in instructori.id_instructor%type ,pSalMediu in out number)
-is
-nume instructori.nume%type;
-salariu instructori.sal%type;
-begin
-select sal into salariu
-from instructori where id_instructor=Pid_Instructor;
-if salariu<pSalMediu then
-  ModificaSalariul(Pid_Instructor,15);
- end if;
- CalcSalMediu(pSalMediu);
+select nume into varNume from instructori where id_instructor=15;
+dbms_output.put_line(varNume);
+EXCEPTION
+when no_Data_found then
+dbms_output.put_line('Instructor cu id invalid');
 end;
 /
-variable SalTemp number;
-call  Modif(2,:SalTemp);
-print SalTemp;
- 
 
---5. Cauta instructorul dupa id
-SET SERVEROUTPUT ON;
-create or replace procedure cauta_Instructor (pIDINS instructori.id_instructor%type,pNume out instructori.nume%type,vSal out instructori.sal%type)
-is
-BEGIN
-select nume,sal into  pNume, vSal
-from instructori
-where id_instructor=pIDINS;
 
-dbms_output.put_line(pNume|| ' '||vSal);
-END;
-/
---Apel 1
-variable Nume varchar;
-variable Sal number
-call cauta_Instructor(1,:Nume,:Sal);
+--2. Să se afişeze salariul instructorului cu numele Chelcea. Să se trateze eroare apărută în cazul în care există mai mulţi
+--instructori cu acelaşi nume 
 
---Apel 2
-Declare 
-pNume1  instructori.nume%type;
-vSal1  instructori.sal%type;
-BEGIN 
-cauta_Instructor(1,pNume1,vSal1);
-END; 
-/
-
- 
-
--- 6. Apelul procedurii intr-un bloc anonim
-SET SERVEROUTPUT ON
-DECLARE
-vID_instructor instructori.id_instructor%type;
+update instructori set nume='Chelcea' where nume='Bogdan';
+set serveroutput on;
+declare 
 vnume instructori.nume%type;
-salar instructori.sal%type;
-VSalMed number;
-BEGIN
-vID_instructor:=1;
-cauta_Instructor(vID_instructor,vnume,salar);
-CalcSalMediu(VSalMed);
-dbms_output.put_line('Salariul mediu este: '||VSalMed);
-Modif(vID_instructor,VSalMed);
-Cauta_Instructor(vID_instructor,vnume,salar);
-vID_instructor:=100;
-Modif(vID_instructor,VSalMed);
+vsal instructori.sal%type;
+begin
+select nume,sal into vnume,vsal from instructori where nume='Chelcea';
+dbms_output.put_line(vnume||' '||vsal);
 EXCEPTION
-when no_data_found then
-DBMS_OUTPUT.PUT_LINE('Instructor inexistent! ID invalid');
-END; 
+ when too_many_rows
+ then 
+ dbms_output.put_line(' Au fost returnate prea multe randuri');
+end;
 /
 
- 
 
--- 7. Stergerea unei Proceduri
-DROP PROCEDURE Modif;
- 
---8. Vizualizarea procedurilor
-Select object_name
-From user_objects
-Where object_type='PROCEDURE';
- 
---9. Vizualizarea corpului procedurii
-Select text
-From user_source
-Where name='Cauta_Instructor' and type='PROCEDURE';
-
-
-
--- FUNCTII
-
-
-
---10 .Func?ia verifica_salariul returneaz? TRUE/FALSE daca salariatul are salariul mai mare/mai mic sau egal cu salariul mediu  si NULL daca salariatul nu exista
-
-create or replace function verifica_salariul(pID_instructor in instructori.id_instructor%type,pSal in instructori.sal%type)
-return Boolean
-IS
-vSal instructori.sal%type;
-BEGIN
-select sal into Vsal from instructori where id_instructor=pID_instructor;
-if Vsal<Psal then
-return true;
-else
-return false;
+--3. Incercarea deschiderii unui cursor deja deschis
+declare
+cursor myC is
+select * from instructori;
+begin
+if not myC%isopen
+then
+open myC;
 end if;
+for i in myC
+loop
+dbms_output.put_line(i.nume);
+exit when myC%notfound;
+end loop;
 EXCEPTION
-WHEN no_data_found THEN
-return NULL;
+when CURSOR_ALREADY_OPEN 
+then
+dbms_output.put_line(' Cursor deja deschis');
+end;
+/
+
+--4. Exceptie la stergere
+CREATE TABLE erori 
+(utilizator VARCHAR2(40), 
+data DATE, 
+cod_eroare NUMBER(10), 
+mesaj_eroare VARCHAR2(255)
+);
+
+DECLARE
+cod NUMBER;
+mesaj VARCHAR2(255);
+del_exception   EXCEPTION;
+PRAGMA EXCEPTION_INIT(del_exception, -2292);
+BEGIN
+DELETE FROM instructori;
+EXCEPTION
+WHEN del_exception THEN
+dbms_output.put_line('Nu puteti sterge instructorii');
+cod:=SQLCODE;
+mesaj:=SQLERRM;
+INSERT INTO erori VALUES(USER, SYSDATE, cod, mesaj);
+END;
+/
+SELECT * FROM erori;
+
+--5.  Să se modifice sal instructorului cu id-ul 65. 
+--Dacă nu se produce nici o actualizare sa se afiseze o excepţie prin care să fie avertizat utilizatorul:
+DECLARE
+ID_invalid EXCEPTION;
+BEGIN
+UPDATE instructori 
+SET sal=100
+WHERE id_instructor=65;
+IF SQL%NOTFOUND THEN
+RAISE ID_invalid;
+END IF;
+
+EXCEPTION
+WHEN ID_invalid THEN
+DBMS_OUTPUT.PUT_LINE('Nu exista instructorul cu acest ID');
+WHEN OTHERS THEN
+DBMS_OUTPUT.PUT_LINE('A aparut o eroare! Nu se poate actualiza salariul instructorukui!');
 END;
 /
 
---11. Vizualizarea tipului returnat
-describe verifica_salariul;
- 
 
---12. Apel
+--5.  Să se modifice sal instructorului cu id-ul 65.  Folosind o EXCEPTIE PREDEFINITA NO_DATA-FOUND
+DECLARE
+BEGIN
+UPDATE instructori 
+SET sal=100
+WHERE id_instructor=65;
+IF SQL%NOTFOUND THEN
+RAISE no_Data_found;
+END IF;
+EXCEPTION
+WHEN no_Data_found THEN
+DBMS_OUTPUT.PUT_LINE('Nu exista instructorul cu acest ID');
+WHEN OTHERS THEN
+DBMS_OUTPUT.PUT_LINE('A aparut o eroare! Nu se poate actualiza salariul instructorukui!');
+END;
+/
+
+
+--6.  Raise Exception
+
 SET SERVEROUTPUT ON
 DECLARE
-v_sal_mediu number;
+cod NUMBER(7);
+mesaj VARCHAR2(255);
+invalid_prod EXCEPTION;
+PRAGMA EXCEPTION_INIT(invalid_prod,-20999);
 BEGIN
-CalcSalMediu (v_sal_mediu);
-IF (verifica_salariul(1, v_sal_mediu) IS NULL) then
-dbms_output.put_line('Instructor cu ID invalid!');
-elsif (verifica_salariul(1, v_sal_mediu)) then
-dbms_output.put_line('Instructorul are sal> avg ');
-else
-dbms_output.put_line('Instructorul are sal < avg ');
-end if;
+UPDATE instructori 
+SET sal=100
+WHERE id_instructor=65;
+IF SQL%NOTFOUND THEN
+RAISE_APPLICATION_ERROR (-20999,'ID instructor invalid!');
+END IF;
+EXCEPTION
+WHEN invalid_prod THEN
+DBMS_OUTPUT.PUT_LINE('Nu exista instructorul cu acest ID');
+cod:=SQLCODE;
+mesaj:=SQLERRM;
+INSERT INTO ERORI VALUES(USER, SYSDATE, cod, mesaj);
 END;
 /
 
- 
 
---13 . O alta metoda de folosire a functiilor
-CREATE OR REPLACE FUNCTION SalariuModificat (sal IN NUMBER, proc IN NUMBER)
-RETURN NUMBER 
-IS
-BEGIN
-RETURN (sal*proc/100);
-END SalariuModificat;
-/
-select nume,SalariuModificat(sal,5) as Sal
-from instructori;
 
-   
+
+
+
+
+
+
+
 
 
 
